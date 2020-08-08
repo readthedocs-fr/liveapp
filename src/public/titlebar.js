6886d5658c4264c3c6150c2153d3e336f89785f4
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-08-08 15:41:50 +0200

    Fixes security issues

--- src/public/titlebar.js
+++ src/public/titlebar.js
@@ -1,4 +1,7 @@
-const { ipcRenderer } = require('electron')
+// when no preloading is done, for pages with nodeIntegration enabled
+if (ipcRenderer === undefined) {
+    global.ipcRenderer = require('electron')
+}
 
 document.getElementById('minimize').addEventListener('click', () => ipcRenderer.send('minimizeWindow'))
 document.getElementById('close').addEventListener('click', () => ipcRenderer.send('closeWindow'))
