ae1436fab5e5696b5b56cb0827a67ebd50bf2af0
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-08-08 13:49:30 +0200

    Titlebar is now added by titlebar.js

--- src/public/titlebar.js
+++ src/public/titlebar.js
@@ -3,5 +3,15 @@ if (ipcRenderer === undefined) {
     global.ipcRenderer = require('electron')
 }
 
+document.getElementById('titlebar').innerHTML = `<a>Live App — Read The Docs</a>
+<div class="buttons">
+    <div id="minimize">
+        <img src="assets/fontawesome5/minimize.svg" alt="minimize window">
+    </div>
+    <div id="close">
+        <img src="assets/fontawesome5/close.svg" alt="close window">
+    </div>
+</div>`
+
 document.getElementById('minimize').addEventListener('click', () => ipcRenderer.send('minimizeWindow'))
 document.getElementById('close').addEventListener('click', () => ipcRenderer.send('closeWindow'))
