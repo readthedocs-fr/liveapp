b1b416c159db7d594e2fa4fe704033af62e7bf87
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-08-11 19:53:22 +0200

    Make a basic config window frontend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow, screen, ipcMain, Tray, Menu } = require('electron')
 const path = require('path')
+const { create } = require('domain')
 
-function createWindow() {
+let mainWindow
+let configWindow
+
+function createMainWindow() {
+    if (mainWindow !== undefined) {
+        return
+    }
+
     const { width, height } = screen.getPrimaryDisplay().workAreaSize
 
     const windowWidth = (width / 5 >= 300) ? Math.floor(width / 5) : 300
-    const win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: windowWidth,
         height: height,
         webPreferences: {
@@ -16,21 +24,44 @@ function createWindow() {
         frame: false,
         icon: path.join(__dirname, 'public/assets/logo.png')
     })
-    win.setPosition(width - windowWidth, 0)
-    win.loadFile('public/index.html')
+    mainWindow.setPosition(width - windowWidth, 0)
+    mainWindow.loadFile('public/index.html')
 
-    ipcMain.on('minimizeWindow', () => win.minimize())
-    ipcMain.on('closeWindow', () => win.close())
+    mainWindow.on('closed', () => mainWindow = undefined)
 
-    return win
+    ipcMain.on('minimizeWindow', () => mainWindow.minimize())
+    ipcMain.on('closeWindow', () => mainWindow.close())
+
+    return mainWindow
+}
+
+function createConfigWindow() {
+    if (configWindow !== undefined) {
+        return
+    }
+    
+    configWindow = new BrowserWindow({
+        width: 300,
+        height: 120,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        parent: mainWindow
+    })
+    configWindow.removeMenu()
+    configWindow.center()
+    configWindow.loadFile('public/config.html')
+
+    configWindow.on('closed', () => configWindow = undefined)
 }
 
 app.whenReady().then(() => {
-    const win = createWindow()
+    const win = createMainWindow()
     const tray = new Tray(path.join(__dirname, 'public/assets/logo.png'))
     
     tray.setToolTip('LiveApp')
     tray.setContextMenu(Menu.buildFromTemplate([
+        {label: 'Configure', click: createConfigWindow},
         {label: 'Minimize', click: () => win.minimize()},
         {label: 'Quit', click: () => app.quit()}
     ]))
